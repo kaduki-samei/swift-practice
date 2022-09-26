b907a960fefbf7f164bdef0930f50efcab111f87
kaduki-samei <kaduki0331@gmail.com>
2022-09-26 10:52:52 +0900

    Initial Commit

---
+++ SwiftPractice01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPractice01
+//
+//  Created by かづき on R 4/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
